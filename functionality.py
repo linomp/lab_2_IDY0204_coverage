10ec4839fdf75679d118e46eaa1d9b76ba3c2338
Rajab Azizli <rajabdearly@gmail.com>
2020-10-25 14:54:47 +0200

    added source file

---
+++ functionality.py
@@ -0,0 +1,17 @@
+def array_manipulation ():
+    n, queries = map(int, input().split())
+
+    arr = [0 for i in range(n+2)]
+
+    for i in range(queries):
+        start, finish, k = map(int, input().split())
+        arr[start - 1] += k
+        arr[finish] -= k
+    ans = 0
+    current = 0
+    for i in arr:
+        current += i
+        if current > ans:
+            ans = current
+
+    return ans
